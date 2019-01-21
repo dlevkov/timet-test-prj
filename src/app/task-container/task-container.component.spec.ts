f32a640ffa1b4f55ba173a5eae7612b96c285292
dlevkov <dlevkov@gmail.com>
2019-01-21 23:40:52 +0200

    test fixed

--- src/app/task-container/task-container.component.spec.ts
+++ src/app/task-container/task-container.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskContainerComponent } from './task-container.component';
 import { LogicService } from '../logic.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MinuteSecondsPipe } from '../minute-seconds.pipe';
 
 describe('TaskContainerComponent', () => {
   let component: TaskContainerComponent;
@@ -10,9 +11,7 @@ describe('TaskContainerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        TaskContainerComponent,
-      ],
+      declarations: [TaskContainerComponent, MinuteSecondsPipe],
       providers: [{ provide: LogicService, useValue: jest.fn() }],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
