efe4cde1b52e72d118024aa2bd382eea426bac77
dlevkov <dlevkov@gmail.com>
2019-01-27 22:49:44 +0200

    added submit test

--- src/app/task-add/task-add.component.spec.ts
+++ src/app/task-add/task-add.component.spec.ts
@@ -2,8 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LogicService } from '../logic.service';
 import { TaskAddComponent } from './task-add.component';
-import { MatFormFieldModule, MatCardModule } from '@angular/material';
+import { MatFormFieldModule, MatCardModule, MatInputModule } from '@angular/material';
 import { of } from 'rxjs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 describe('TaskAddComponent', () => {
   let component: TaskAddComponent;
   let fixture: ComponentFixture<TaskAddComponent>;
@@ -20,6 +21,8 @@ describe('TaskAddComponent', () => {
         FormsModule,
         MatFormFieldModule,
         MatCardModule,
+        MatInputModule,
+        NoopAnimationsModule
       ],
     });
     fixture = TestBed.createComponent(TaskAddComponent);
@@ -50,12 +53,13 @@ describe('TaskAddComponent', () => {
       expect(formBuilderStub.group).toHaveBeenCalled();
     });
   });
-  describe.skip('submitHandler', () => {
+  describe('submitHandler', () => {
     it('makes expected calls', () => {
       const logicServiceStub: LogicService = fixture.debugElement.injector.get(
         LogicService
       );
       jest.spyOn(logicServiceStub, 'addTask').mockReturnValue({});
+      fixture.detectChanges();
       component.submitHandler('someText');
       expect(logicServiceStub.addTask).toHaveBeenCalled();
     });
