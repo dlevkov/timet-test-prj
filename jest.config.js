e7f0f1a1c6323d16a2fb34f9d7269b1737798b18
Denis Levkov <denis@Deniss-MacBook-Pro.local>
2019-03-28 23:32:51 +0200

    angular updated, breaked fixed

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   roots: ['src'],
-  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+  setupFilesAfterEnv: '<rootDir>/src/setup-jest.ts',
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@assets/(.*)': '<rootDir>/src/assets/$1',
@@ -16,6 +16,6 @@ module.exports = {
     'src/app/**/*.{ts}',
     '!src/app/**/*.module.{ts}',
     '!<rootDir>/node_modules/',
-    '!<rootDir>/src/',
-  ],
+    '!<rootDir>/src/'
+  ]
 };
